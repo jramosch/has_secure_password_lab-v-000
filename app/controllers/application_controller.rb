5eff49db29ea2d5c6f942ff77eb58f11833021e7
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-03 04:54:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def welcome
+    @user = User.find(session[:user_id])
+    redirect_to '/login' if @user.nil?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to '/'
+  end
+
+  def show
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
